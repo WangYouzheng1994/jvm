658e8ef7d5fe1bb3a76b609816e226c6efd5bafa
wangyouzheng <353735348@qq.com>
2020-01-07 22:59:03 +0800

    数组的类加载器

--- src/main/java/com/wyz/classloader/MyTest15.java
+++ src/main/java/com/wyz/classloader/MyTest15.java
@@ -10,5 +10,15 @@ public class MyTest15 {
 		String[] strings = new String[2];
 		System.out.println(strings);
 		System.out.println(strings.getClass());
+		// 数组的类加载器与数组的类型有关系。当前数组类型为String， String的类加载器是启动类加载器  启动类加载器返回的是null
+		System.out.println(strings.getClass().getClassLoader()); // null
+
+		MyTest15[] myTest15s = new MyTest15[2];
+		System.out.println(myTest15s);
+		System.out.println(myTest15s.getClass().getClassLoader());
+
+		int[] ints = new int[2];
+		System.out.println(ints);
+		System.out.println(ints.getClass().getClassLoader()); // 原生(基本)类型 没有类加载器
 	}
 }
